5910208163725586873bb1f7eb2dcd0ee42c4e2a
AnaduCG <godzionwin@gmail.com>
2023-10-05 14:13:09 +0100

    Creating fiber file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python Fabric script that generates a .tgz archive
+   from the contents of the web_static folder of your
+   AirBnB Clone repo, using the function do_pack
+"""
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(file))
