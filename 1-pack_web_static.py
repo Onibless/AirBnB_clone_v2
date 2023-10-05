c0a86813e5eb5ce2c815809219e2cd22b6c591c9
AnaduCG <godzionwin@gmail.com>
2023-10-05 14:31:19 +0100

    Updating typo

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,3 +11,7 @@ def do_pack():
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     file = "versions/web_static_{}.tgz".format(date)
     result = local("sudo tar -cvzf {} web_static".format(file))
+    if result.succeeded:
+        return file
+    else:
+        return None
