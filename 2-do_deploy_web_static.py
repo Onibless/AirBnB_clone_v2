669349f7fa2437bdb8f297dee406f52a84bf29b9
AnaduCG <godzionwin@gmail.com>
2023-10-13 09:51:56 +0100

    Adding sudo to comands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,13 +23,13 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(extract_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, extract_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(extract_tgz, extract_tgz))
-        run("rm -rf {}/web_static".format(extract_tgz))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(extract_tgz))
+        run("sudo mkdir -p {}/".format(extract_tgz))
+        run("sudo tar -xzf {} -C {}/".format(tmp, extract_tgz))
+        run("sudo rm {}".format(tmp))
+        run("sudo mv {}/web_static/* {}/".format(extract_tgz, extract_tgz))
+        run("sudo rm -rf {}/web_static".format(extract_tgz))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}/ /data/web_static/current".format(extract_tgz))
         return True
     except Exception:
         return False
