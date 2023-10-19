0c0f73fbcf1f20eaf450451c87f85d37a5fc3a7a
AnaduCG <godzionwin@gmail.com>
2023-10-19 14:49:55 +0100

    number validating and url routing

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+A script that satarts a flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Function that returns a string
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Function that returns a string
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Function that returns a string
+    """
+    str = text.replace("_", " ")
+    return f"C {escape(str)}"
+
+@app.route('/python/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """
+    Function that returns a string
+    """
+    str = text.replace("_", " ")
+    return f"Python {escape(str)}"
+
+# @app.route('/number/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Function that returns a string
+    """
+    return f"{escape(n)} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=None)
