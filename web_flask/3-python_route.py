3fcee9a31f064b5eae94f5c3d204898ef198a659
AnaduCG <godzionwin@gmail.com>
2023-10-19 14:49:30 +0100

    python  url routing

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+A script that satarts a flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Function that returns a string
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Function that returns a string
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Function that returns a string
+    """
+    str = text.replace("_", " ")
+    return f"C {escape(str)}"
+
+@app.route('/python/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """
+    Function that returns a string
+    """
+    str = text.replace("_", " ")
+    return f"Python {escape(str)}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=None)
