b391b6cea3dcc86496463d3b7f0efa433d876180
AnaduCG <godzionwin@gmail.com>
2023-10-20 13:40:57 +0100

    Handling database

--- models/place.py
+++ models/place.py
@@ -4,32 +4,35 @@ import os
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.review import Review
 from models.amenity import Amenity
+
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table(
+            'place_amenity',
+            Base.metadata,
+            Column(
+                'place_id',
+                String(60),
+                ForeignKey('places.id'),
+                nullable=False,
+                primary_key=True
+                ),
+            Column(
+                'amenity_id',
+                String(60),
+                ForeignKey('amenities.id'),
+                nullable=False,
+                primary_key=True
+                )
+            )
 
 
 class Place(BaseModel):
     """ A place to stay """
-    place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        nullable=False,
-        primary_key=True
-    ),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        nullable=False,
-        primary_key=True
-    )
-)
-"""Represents the many to many relationship table
+
+    """Represents the many to many relationship table
 between Place and Amenity records.
 """
 
@@ -38,48 +41,48 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(
-        String(60), ForeignKey('cities.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+            String(60), ForeignKey('cities.id'), nullable=False
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     user_id = Column(
-        String(60), ForeignKey('users.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+            String(60), ForeignKey('users.id'), nullable=False
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+            String(128), nullable=False
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     description = Column(
-        String(1024), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+            String(1024), nullable=True
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     number_rooms = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+            Integer, nullable=False, default=0
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
     number_bathrooms = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+            Integer, nullable=False, default=0
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
     max_guest = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+            Integer, nullable=False, default=0
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
     price_by_night = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+            Integer, nullable=False, default=0
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
     latitude = Column(
-        Float, nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
+            Float, nullable=True
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
     longitude = Column(
-        Float, nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
+            Float, nullable=True
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
     amenity_ids = []
     reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='place'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+            'Review',
+            cascade="all, delete, delete-orphan",
+            backref='place'
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         amenities = relationship(
-            'Amenity',
-            secondary=place_amenity,
-            viewonly=False,
-            backref='place_amenities'
-        )
+                'Amenity',
+                secondary=place_amenity,
+                viewonly=False,
+                backref='place_amenities'
+                )
     else:
         @property
         def amenities(self):
