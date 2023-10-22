a7eecf9cb96a102bf972bb1f550f0cc562a21c1c
AnaduCG <godzionwin@gmail.com>
2023-10-22 11:16:17 +0100

    Python docstring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import render_template
-from markupsafe import escape
+# from markupsafe import escape
 from models import storage
 
 
@@ -8,13 +8,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", stirct_slashes=False)
 def states_list():
-    """"""
+    """Fetches state table data"""
     states = storage.all()
     return(render_template('7-states_list.html', state=states))
 
 @app.teardown_appcontext
-def teardow(self):
-    """"""
+def teardown(self):
+    """closes sqlalchemy session
+    """
     storage.close()
 
 if __name__ == "__main__":
