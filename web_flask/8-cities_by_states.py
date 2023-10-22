ed6af24f31ebcc87a393def311d240e0f42854eb
AnaduCG <godzionwin@gmail.com>
2023-10-22 15:40:54 +0100

    Sortting data

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,2 +1,23 @@
 #!/usr/bi/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Fetches states and cities"""
+    states = state.all()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Closes all sqlalchemy session"""
+    storage.close()
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=5000)
